90df84d84f92304c5d28fb69bc34fc17ed22807b
Christina Lu <c.20@dartmouth.edu>
2023-03-23 15:27:57 -0700

    initial skeleton for website

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+// // HEADER SCROLL
+// window.addEventListener('scroll', onHeaderScroll);
+// title = document.getElementById("title");
+//
+// function onHeaderScroll () {
+//     var scrollPosition = window.scrollY,
+//         showHeaderPosition = 680;
+//     // Determine if position is at a certain point
+//     if (scrollPosition >= showHeaderPosition) {
+//         showHeader();
+//     } else {
+//         hideHeader();
+//     }
+// }
+//
+// function showHeader() {
+//   title.style.display = "block";
+// }
+//
+// function hideHeader() {
+//   title.style.display = "none";
+// }
