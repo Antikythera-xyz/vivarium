a403a8e8a4d01333aa9d2b1088fd5e0ed8f67f59
Christina Lu <c.20@dartmouth.edu>
2023-03-29 23:45:33 -0700

    add vivarium section image for all game sections

--- js/scroll.js
+++ js/scroll.js
@@ -1,23 +1,66 @@
 // Game grid SCROLL
 window.addEventListener('scroll', onGameGridScroll);
 gameGrid = document.getElementById("vivarium");
-blurHere = document.getElementById("vivariumBlur");
+gridBlur = document.getElementById("vivariumBlur");
 
 function onGameGridScroll () {
     var scrollPosition = window.scrollY;
     var scrollTop = document.body.scrollTop + 3*window.innerHeight/4;
     // Determine if position is at a certain point
-    if (scrollTop > blurHere.getBoundingClientRect().top) {
+    if (scrollTop > gridBlur.getBoundingClientRect().top) {
         gameGrid.classList.add("blur");
     } else {
         gameGrid.classList.remove("blur");
     }
 }
-//
-// function showHeader() {
-//   title.style.display = "block";
-// }
-//
-// function hideHeader() {
-//   title.style.display = "none";
-// }
+
+// Sensorimotor SCROLL
+window.addEventListener('scroll', onSensorimotorScroll);
+ss = document.getElementById("sensorimotor");
+ssBlur = document.getElementById("sensorimotorBlur");
+
+function onSensorimotorScroll () {
+    var scrollPosition = window.scrollY;
+    var scrollTop = document.body.scrollTop + window.innerHeight/2;
+    // Determine if position is at a certain point
+    if (scrollTop > ssBlur.getBoundingClientRect().top) {
+        ss.classList.add("blur");
+    } else {
+        ss.classList.remove("blur");
+    }
+}
+
+
+// Platformer SCROLL
+window.addEventListener('scroll', onPlatformerScroll);
+p = document.getElementById("platformer");
+pBlur = document.getElementById("platformerBlur");
+
+function onPlatformerScroll () {
+    var scrollPosition = window.scrollY;
+    var scrollTop = document.body.scrollTop + window.innerHeight/2;
+    // Determine if position is at a certain point
+    if (scrollTop > pBlur.getBoundingClientRect().top) {
+        p.classList.add("blur");
+    } else {
+        p.classList.remove("blur");
+    }
+}
+
+
+
+// MMARPG SCROLL
+window.addEventListener('scroll', onMMARPGScroll);
+m = document.getElementById("mmarpg");
+mBlur = document.getElementById("mmarpgBlur");
+
+function onMMARPGScroll () {
+    var scrollPosition = window.scrollY;
+    var scrollTop = document.body.scrollTop + window.innerHeight/2;
+    // Determine if position is at a certain point
+    if (scrollTop > mBlur.getBoundingClientRect().top) {
+        m.classList.add("blur");
+    } else {
+        m.classList.remove("blur");
+    }
+}
