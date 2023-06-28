6c577f3f7050d7095f7a8cde352c00f801c46f50
dalena <dalenaxtran@gmail.com>
2023-06-28 02:05:43 -0700

    changed scroll duration

--- js/scroll.js
+++ js/scroll.js
@@ -3,7 +3,7 @@ $(function() {
     $(window).scroll(function() {    
         var scroll = $(window).scrollTop();
     
-        if (scroll >= 200) {
+        if (scroll >= 350) {
             header.removeClass('header').addClass("header-alt");
         } else {
             header.removeClass("header-alt").addClass('header');
