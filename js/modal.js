28ee7d814cb6da66cd2395d0dfeb1a1ad518fc7c
Christina Lu <christina.ty.lu@gmail.com>
2023-08-21 18:39:16 +0100

    add modal for categories and clean up unnecessary files

---
+++ js/modal.js
@@ -0,0 +1,26 @@
+// Modal and button IDs abbreviations should be added to categories.
+
+var categories = ["ss", "cn", "cs", "oc", "wc", "mm"];
+
+for (var cat of categories) {
+  let modal = document.getElementById(cat + "Modal");
+  let btn = document.getElementById(cat + "Btn");
+  let close = document.getElementById(cat + "Close");
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "flex";
+  }
+
+  // When the user clicks on (x), close the modal
+  close.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
